2e5c0cbd0589e95689c58318fccb97b44368e67a
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-05-03 11:17:59 +0530

    Add files via upload

---
+++ Module4/3-5-sqflite/screens/contactlist.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatefulWidget
+{
+  const ContactList({super.key});
+
+  @override
+  State<ContactList> createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Placeholder();
+  }
+}
