5ceb7fbb55cf954d47e8dfea6d06c2a8eaef9edd
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-06-24 11:24:48 +0530

    commit

--- Project/project123/lib/main.dart
+++ Project/project123/lib/main.dart
@@ -1,17 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project123/splashscreen.dart';
+import 'package:project123/screens/admin/bottom_navigation/upload/upload.dart';
+import 'package:project123/screens/admin/front/front.dart';
+import 'package:project123/screens/user/splash_screen/splash_screen.dart';
 
 void main()
 {
-  runApp(MaterialApp
-    (
-      theme: ThemeData
-        (
-          fontFamily:AutofillHints.language,
-          backgroundColor: Colors.blueAccent
-        ),
-      debugShowCheckedModeBanner: false,
-      home:SplashScreen()
-    ));
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+      (
+      debugShowCheckedModeBanner: false,
+      title: 'E wishes',
+      home: SplashScreen(),
+    );
+  }
+}
